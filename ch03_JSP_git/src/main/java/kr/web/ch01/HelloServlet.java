6e60a22d9cfe57563dc868326b860969459f3f35
minji0626 <jo010163@naver.com>
2024-04-19 13:55:05 +0900

    .

---
+++ ch03_JSP_git/src/main/java/kr/web/ch01/HelloServlet.java
@@ -0,0 +1,33 @@
+package kr.web.ch01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// 일반 클래스를 서블릿으로 사용할 경우 HttpServlet를 상속시켜야 한다.
+@WebServlet("/helloServlet")
+public class HelloServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+
+//		문서 타입 및 캐릭터셋 지정
+		response.setContentType("text/html;charset=utf-8");
+		
+//		HTML 출력을 위한 출력 스트림 생성
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head><title>Hello Servlet</title></head>");
+		out.println("<body>");
+		out.println("처음 작성하는 servlet 입니다.");
+		out.println("</body>");
+		out.println("</html>");
+		   
+//		자원 정리
+		out.close();
+	}
+
+}
---
+++ ch03_JSP_git/src/main/java/kr/web/ch01/NowServlet.java
@@ -0,0 +1,38 @@
+package kr.web.ch01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// 일반 클래스를 서블릿으로 사용할 경우 HttpServlet를 상속시켜야 한다.
+@WebServlet("/now")
+public class NowServlet extends HttpServlet {
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		Date now = new Date();
+		SimpleDateFormat sf = new SimpleDateFormat("yyyy년MM월dd일 a hh:mm:ss");
+		
+//		문서 타입 및 캐릭터셋 지정
+		response.setContentType("text/html;charset=utf-8");
+		
+//		HTML 출력을 위한 출력 스트림 생성
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head><title>현재 날짜와 시간</title></head>");
+		out.println("<body>");
+		out.println("현재 날짜와 시간 : " + sf.format(now));
+		out.println("</body>");
+		out.println("</html>");
+		
+//		자원 정리
+		out.close();
+	}
+
+}
