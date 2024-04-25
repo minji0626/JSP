4ce571ea199a2a1837412995b1692ac2f83774f2
minji0626 <jo010163@naver.com>
2024-04-25 22:52:51 +0900

    .

--- ch03_JSP_git/src/main/java/kr/util/DBUtil.java
+++ ch03_JSP_git/src/main/java/kr/util/DBUtil.java
@@ -7,7 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DBUtil {
-	
 	private static final String DB_DRIVER = "oracle.jdbc.OracleDriver";
 	private static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:xe";
 	private static final String DB_ID = "user01";
@@ -20,7 +19,7 @@ public class DBUtil {
 //		JDBC 수행 2단계
 		return DriverManager.getConnection(DB_URL,DB_ID, DB_PASSWORD);
 	}
-	
+	 
 //	자원 정리
 	public static void executeClose(ResultSet rs, PreparedStatement pstmt, Connection conn) {
 		if(rs!=null) try {rs.close();} catch(SQLException e) {}
